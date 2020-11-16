f896653aed8442d6d4b2b49175b257f79b58e15f
hanxiangxu <microhan2019@gmail.com>
2020-11-17 02:46:57 +0800

    新分支1

--- src/main/java/NewTest.java
+++ src/main/java/NewTest.java
@@ -1,5 +1,10 @@
 public class NewTest {
     public static void main(String[] args) {
         System.out.println("新分支");
+        System.out.println("新分支1");
+        System.out.println("新分支2");
+        System.out.println("新分支3");
+        System.out.println("新分支4");
+
     }
 }
